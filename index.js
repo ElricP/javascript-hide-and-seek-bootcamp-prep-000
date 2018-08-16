1e5c903cca476a6c2134f14321c2ba69a5c0fec5
Yuan Pan <y39pan@edu.uwaterloo.ca>
2018-08-16 14:59:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,5 +8,9 @@ function nestedTarget(){
 }
 
 function increaseRankBy(n){
-  
+  const rankedList = document.querySelectorAll("ul.ranked-list li");
+  for(let i = 0; i < rankedList.length; i++){
+    rankedList[i] = parseInt(rankedList[i]) + n;
+  }
+  return rankedList;
 }
