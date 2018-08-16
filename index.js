69ed1841492c707d6f2b5157310f8d9456ffe66c
Yuan Pan <y39pan@edu.uwaterloo.ca>
2018-08-16 15:18:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  return document.querySelector("#nested .target");
+}
+
+function increaseRankBy(n){
+  const rankedList = document.querySelectorAll("ul.ranked-list li");
+  for(let i = 0; i < rankedList.length; i++){
+    rankedList[i].innerHTML = parseInt(rankedList[i].innerHTML) + n;
+  }
+  return rankedList;
+}
+
+function deepestChild(){
+  var index = "#grand-node";
+  var current = document.querySelector(index);
+  while(document.querySelector(index+" div") !== null){
+    index +=" div";
+  }
+  current = document.querySelector(index);
+  return current;
+}
